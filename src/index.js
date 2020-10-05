b0b57d6cc99315ffff846fe615816f0948e5ea69
Miraslau Rabikau <31839505+Miroslau@users.noreply.github.com>
2020-10-05 18:25:38 +0300

    fix: changed index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 module.exports = function reverse (n) {
-  n += "";
-  n = n.split("");
-  n = n.reverse().join("");
-  let result = +n;
-  return result;
+  let result;
+  if(n > 0) {
+    n += "";
+    n = n.split("");
+    result = n.reverse().join("");
+  } else {
+    n += "";
+    n = n.split("");
+    n.shift();
+    result = n.reverse().join("");
+  }
+  return +result;
 }
